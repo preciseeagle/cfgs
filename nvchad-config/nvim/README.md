0eeeddf89c06aa6df089fec6a947f2b3537eace6
Derek <dphamcasd@gmail.com>
2024-05-17 16:20:08 -0700

    add nvchad nvim config folder

---
+++ nvchad-config/nvim/README.md
@@ -0,0 +1,7 @@
+# starter
+Starter config for NvChad
+
+# setting up this to work on my chromebook
+Created a display-line navigation in ./lua/mappings.lua
+
+Disabled mouse support in init.lua so that neovim does not treat mouse as part of container's clipboard and will allow chromeos to paste text into the editor
---
+++ nvchad-config/nvim/init.lua
@@ -0,0 +1,54 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+-- MY STUFF --
+-- disable neovim mouse support on chromeos
+-- vim.opt.mouse = ""
+
+-- set indenting options
+local indent = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+--vim.opt.tabstop = indent
+--vim.opt.softtabstop = indent
+--vim.opt.shiftwidth = indent
+
+-- enable true color terminal
+vim.opt.termguicolors = true
