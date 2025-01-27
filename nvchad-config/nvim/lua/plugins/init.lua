0a5ea630ead5f0f4dacda13dcae52f0be23b4ae5
Derek <dphamcasd@gmail.com>
2025-01-27 03:20:02 -0800

    installed solargraph to nvim config and clean

--- nvchad-config/nvim/lua/plugins/init.lua
+++ nvchad-config/nvim/lua/plugins/init.lua
@@ -8,24 +8,23 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
+  {
+    "neovim/nvim-lspconfig",
+ --   enabled = false,
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"prettier", "solargraph"
+  		},
+  	},
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
